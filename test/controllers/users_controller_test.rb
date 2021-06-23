ee54b456686616d9cf90a316748e2def1a01fe19
Sergei Bronnikov <SeVBronnikov@gmail.com>
2021-06-23 21:57:50 +0300

    Problem ApplicationHelperTest

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
-  test "should get signup" do
+  test "should get new" do
     get :new
     assert_response :success
     assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
