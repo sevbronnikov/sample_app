ebe525643c56bb19ed01682c3c8900a80a254c62
Sergei Bronnikov <SeVBronnikov@gmail.com>
2021-07-04 16:23:13 +0300

    Add registration form and integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-  end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user # Or "redirect_to user_url(@user)"
+      else
+        render 'new'
+      end
+    end
+
+    private
+
+      def user_params
+       params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+        end
+
+end
