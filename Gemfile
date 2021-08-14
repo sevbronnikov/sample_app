ccd5db6c853a9d9e7a024c0e687625130902c3a6
Sergei Bronnikov <SeVBronnikov@gmail.com>
2021-08-14 13:44:00 +0300

    Creating microposts

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   # Use sqlite3 as the database for Active Record
   #gem 'sqlite3', '~> 1.4'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
   gem 'rails-controller-testing'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
     def show
       @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
     end
   
     def new
@@ -52,15 +53,6 @@ class UsersController < ApplicationController
     end
 
     # Предфильтры
-
-    # Подтверждает вход пользователя
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Подтверждает правильного пользователя
     def correct_user
